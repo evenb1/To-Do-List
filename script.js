df34b8e5a753394d74133f48fba801efde4fcb19
Even <even.berhane2022@gmail.com>
2024-04-30 23:02:06 +0300

    variable initializations

---
+++ script.js
@@ -0,0 +1,8 @@
+let addButton = document.querySelector('button');
+let inputValue = document.querySelector('input').value;
+let myDiv = document.getElementsByClassName('todoContainer');
+
+addButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = `- ${}`
+})
